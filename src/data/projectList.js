9d3a56fee9967004906f5cef0937adbb0c531e9f
yass5602 <yasinchk57@gmail.com>
2025-06-24 03:01:25 +0200

    Ajout des images dans les cards de project avec un dossier images assigné

--- src/data/projectList.js
+++ src/data/projectList.js
@@ -1,11 +1,14 @@
 import Logo from '../images/Logo.png';
+import Logo2 from '../images/Sophie_Bluel.png'
+import Logo3 from '../images/Booki.png'
+import Logo4 from '../images/logo-kasa-accueil.png'
 
 export const projects = [
     {
       id: 1,
       title: "Booki",
       description: "Intégration web avec HTML - CSS",
-      image: "https://www.sullivan-strazel.fr/wp-content/uploads/2021/11/banner-booki.jpg",
+      image: Logo3,
       github: "https://github.com/yass5602/Booki",
       skills: ["HTML", "CSS", "Responsive"],
     },
@@ -13,7 +16,7 @@ export const projects = [
       id: 2,
       title: "Portfolio - Sophie Bluel",
       description: "Développement web avec JavaScript",
-      image: "https://codesandbox.io/api/v1/sandboxes/hxf3v4/screenshot.png",
+      image: Logo2,
       github: "https://github.com/yass5602/Portfolio-Sophie-Bluel",
       skills: ["HTML", "CSS", "Javascript"],
     },
@@ -29,7 +32,7 @@ export const projects = [
       id: 4,
       title: "Kasa",
       description: "React",
-      image: "https://camo.githubusercontent.com/bd959cb4594b60ec8f3df8a085323eec25a6e97c49d73bc341698d234abdf2ab/68747470733a2f2f757365722e6f632d7374617469632e636f6d2f75706c6f61642f323032322f30362f32342f31363536303839393736393930365f46525f3831315f50385f42616e6e65722d4b617361253230253238312532392e706e67",
+      image: Logo4,
       github: "https://github.com/yass5602/Projet-Kasa",
       skills: ["Create React App", "Vite", "React"],
     },
